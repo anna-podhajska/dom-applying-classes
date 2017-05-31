97498ddb72b9efcaa6a8f0e0b7c37ceeb4b466ee
Anna P <podhajskaa@gmail.com>
2017-06-01 11:11:30 +1200

    added parametrized functionrefactored - added 1 parametrized func instead 3 similar functions

--- classes.js
+++ classes.js
@@ -1,38 +1,44 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls
-// the start function. More info:
+// Don't change or delete this line! It waits until the DOM has loaded, then calls the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
+
   one()
   two()
   three()
   makeVisible()
+
+  // all('one', 'blue');
+  // all('two', 'green');
+  // all('three', 'orange');
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
   // First, we have to find the element:
   var one = document.getElementById('one')
-
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
 function two () {
   var one = document.getElementById('two')
   one.classList.add('green')
 }
-// CREATE FUNCTION three HERE
 function three () {
   var one = document.getElementById('three')
   one.classList.add('orange')
 }
-
-// CREATE FUNCTION makeVisible HERE
 function makeVisible () {
   var one = document.getElementsByClassName('invisible')[0]
   one.classList.add('visible')
+}
+
+// ___TESTING how to get the first three parametrized
+function all(elemID, color) {
+  var one = document.getElementById(elemID)
+  one.classList.add(color)
 }
